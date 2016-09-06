98622720502a86e7497b30ce8844237c42f89ddd
otnielyehezkiel <striketokill@gmail.com>
2016-09-06 18:14:20 +0700

    fix bug

--- application/views/advice_planning.php
+++ application/views/advice_planning.php
@@ -43,21 +43,21 @@
             <div class="row centered">
                 <div class="col-md-4">
                     <i class="fa fa-book"></i>
-                    <h4>Fitur Advice Planning</h4>
+                    <h4 style="color:#00b3fe;">Fitur Advice Planning</h4>
                     <p>AP atau Advice Planning adalah suatu Pertimbangan Teknis Perencanaan untuk setiap orang pribadi dan atau Badan Usaha yang akan mengajukan Ijin Pemanfaatan Ruang dengan luas lahan minimal 500 (lima ratus) meter persegi. </p>
                     <p>
                     <a href="#" class="btn btn-theme">Regulasi</a></p>
                 </div>
                 <div class="col-md-4">
                     <i class="fa fa-male"></i>
-                    <h4>Form AP Perseorangan</h4>
+                    <h4 style="color:#00b3fe;">Form AP Perseorangan</h4>
                     <p>Pertimbangan Teknis Perencanaan untuk setiap orang pribadi mengajukan Ijin Pemanfaatan Ruang dengan luas lahan minimal 500 (lima ratus) meter persegi.</p>
                     <p><br/>
                     <a href="<?= base_url()?>homepage/form_perseorangan" class="btn btn-theme">Ajukan Form</a></p>
                 </div>
                 <div class="col-md-4">
                     <i class="fa fa-university" aria-hidden="true"></i>
-                    <h4>Form AP Badan Usaha</h4>
+                    <h4 style="color:#00b3fe;">Form AP Badan Usaha</h4>
                     <p>Pertimbangan Teknis Perencanaan untuk Badan Usaha yang akan mengajukan Ijin Pemanfaatan Ruang dengan luas lahan minimal 500 (lima ratus) meter persegi.</p>
                     <p><br/>
                     <a href="<?= base_url()?>homepage/form_badan" class="btn btn-theme">Ajukan Form</a></p>
